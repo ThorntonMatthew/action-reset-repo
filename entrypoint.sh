1ffe7adba76c9a1f7af8b60eaabdf6cd07bbc8a5
Matthew Thornton <99351305+ThorntonMatthew@users.noreply.github.com>
2026-03-18 09:37:08 -0400

    Update GitHub remote URL and email configuration

--- entrypoint.sh
+++ entrypoint.sh
@@ -23,9 +23,9 @@ mkdir _tmp && cd _tmp
 git init
 
 git config user.name "${GITHUB_ACTOR}"
-git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
+git config user.email "${GITHUB_ACTOR}@users.noreply.va.ghe.com"
 
-git remote add origin "https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+git remote add origin "https://x-access-token:${GITHUB_TOKEN}@va.ghe.com/${GITHUB_REPOSITORY}.git"
 
 git fetch
 
